6d7ffafb642e26ccb7febb29d077e0330fc05afa
Edilberto Souza <edilberto.sss@gmail.com>
2022-11-17 00:09:50 -0300

    Inicio definicao da visualização do detalhe

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Trilha JS Developer - Pokedex
+
+https://diniz-pokedex.netlify.app/#2
+https://pokedexlucascordeiro.netlify.app
+https://github.com/JoaoLucasCordeiro/Pokedex
--- assets/js/main.js
+++ assets/js/main.js
@@ -10,14 +10,11 @@ function convertPokemonToLi(pokemon) {
         <li class="pokemon ${pokemon.type}" onclick="showItem(${pokemon.number})">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
-
             <div class="detail">
                 <ol class="types">
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -46,8 +43,85 @@ loadMoreButton.addEventListener('click', () => {
     }
 })
 
+/*
+<li class="pokemon ${pokemon.type}" onclick="showItem(${pokemon.number})">
+<span class="number">#${pokemon.number}</span>
+<span class="name">${pokemon.name}</span>
+<div class="detail">
+    <ol class="types">
+        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+    </ol>
+    <img src="${pokemon.photo}" alt="${pokemon.name}">
+</div>
+</li>
+    <div id ="pokemon-about" class="content pokemon-about" style ="${handleTypeColor(pokemon.types[0].type.name)}">
+
+*/
+function convertPokemonToDiv(pokemonItem) {
+    return `
+    <div class="pokemon-item ${pokemonItem.type}">
+        <div class="nav">
+            <div class="pokemon-item-nav" id="pokemon-item-nav" onClick="backList()">
+                <img width="40px" height="40px" src="/assets/img/back.png" onClick="backList()">
+                    <i class="fa-solid fa-arrow-left-long"></i>
+                </button>    
+            </div>
+        </div>
+        <div class="pokemon-item">    
+            <span class="name">${pokemonItem.name}   #${pokemonItem.number}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemonItem.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+            </div>
+            <img class="pokemon-item-img" width="200px" height="200px" src="${pokemonItem.photo}" alt="${pokemonItem.name}">
+        </div>
+        <div class="pokemon-info">
+            <div id="poke-header">
+                    <ul class="pokemon-item-nav">
+                    <li class="active t-about"> About</li>
+                    <li class="t-base">Base Status</li>
+                </ul>
+            </div>
+            <div id="info-content">
+                <ul>
+                    <li class="active t-about">
+                        <div class="d-flex" id="poke-info">
+                            <ol class="details-a">
+                                <li>Height</li>
+                                <li>Weight</li>
+                                <li>Abilities</li>
+                            </ol>
+                            <ol class="details-b">
+                                <li> ${(pokemonItem.height)/10} cm</li>
+                                <li>${(pokemonItem.weight)/10} kg</li>
+                                    ${pokemonItem.abilities.map((ability) => `<li class="ability ${ability}">${ability}</li>`).join('')}                                    
+                                </li>
+                            </ol>
+                        </div>
+                    </li>
+                </ul>    
+            </div>
+        </div>
+    </div>
+    `
+}
+
 function showItem(number) {
     pokeApi.getPokemonItem(number).then((pokemonItem = '') => {
-        console.log(pokemonItem)
+        //console.log(pokemonItem)
+        //document.getElementById('header').style.display = "none";
+        document.getElementById('content').style.display = "none";
+        document.getElementById('pokemon-content').style.display = "block";
+    
+        location.href="#pokemon-about";
+        document.getElementById('pokemon-content').innerHTML = convertPokemonToDiv(pokemonItem)
+
+        scroll(0,0);    
     })
+}
+
+function backList() {
+    document.getElementById('pokemon-content').style.display = "none";
+    document.getElementById('content').style.display = "block";
 }
