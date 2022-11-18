50cb629c205748382f276222ecc9676937cde073
Edilberto Souza <edilberto.sss@gmail.com>
2022-11-18 18:57:00 -0300

    Concluindo o estilo do detalhe

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,20 +43,6 @@ loadMoreButton.addEventListener('click', () => {
     }
 })
 
-/*
-<li class="pokemon ${pokemon.type}" onclick="showItem(${pokemon.number})">
-<span class="number">#${pokemon.number}</span>
-<span class="name">${pokemon.name}</span>
-<div class="detail">
-    <ol class="types">
-        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-    </ol>
-    <img src="${pokemon.photo}" alt="${pokemon.name}">
-</div>
-</li>
-    <div id ="pokemon-about" class="content pokemon-about" style ="${handleTypeColor(pokemon.types[0].type.name)}">
-
-*/
 function convertPokemonToDiv(pokemonItem) {
     return `
     <div class="pokemon-item ${pokemonItem.type}">
@@ -79,36 +65,23 @@ function convertPokemonToDiv(pokemonItem) {
         <div class="pokemon-info">
             <div id="poke-header">
                 <ul class="pokemon-item-nav">
-                    <li id="t-about" class="active t-about" onClick="showItemAbout()">About</li>
-                    <li id="t-base" class="t-base" onClick="showItemBase()">Base Status</li>
+                    <li id="t-about" class="active t-about">About</li>
                 </ul>
             </div>
             <div id="info-content">
                 <ul>
-                    <li class="active t-about">
-                        <div class="d-flex" id="poke-info">
+                    <li id="t-about-info" class="active t-about">
+                        <div class="d-flex">
                             <ol class="details-a">
-                                <li>Height</li>
-                                <li>Weight</li>
-                                <li>Abilities</li>
+                                <li class="t-about">Height</li>
+                                <li class="t-about">Weight</li>
+                                <li class="t-about">Abilities</li>
                             </ol>
                             <ol class="details-b">
-                                <li> ${(pokemonItem.height)/10} cm</li>
-                                <li>${(pokemonItem.weight)/10} kg</li>
-                                    ${pokemonItem.abilities.map((ability) => `<li class="ability ${ability}">${ability}</li>`).join('')}                                    
+                                <li class="t-about"> ${((pokemonItem.height)/10).toFixed(2)} cm</li>
+                                <li class="t-about">${(pokemonItem.weight)/10} kg</li>
+                                    ${pokemonItem.abilities.map((ability) => `<li class="t-about" ${ability}">${ability}</li>`).join('')}                                    
                                 </li>
-                            </ol>
-                        </div>
-                    </li>
-                    <li class="active t-base">
-                        <div class="d-flex" id="poke-info">
-                            <ol class="details-a">
-                                <li>Alguma coisa</li>
-                                <li>Mais alguma</li>
-                            </ol>
-                            <ol class="details-b">
-                                <li>Info 1</li>
-                                <li>Info 2</li>
                             </ol>
                         </div>
                     </li>
@@ -122,7 +95,6 @@ function convertPokemonToDiv(pokemonItem) {
 function showItem(number) {
     pokeApi.getPokemonItem(number).then((pokemonItem = '') => {
         //console.log(pokemonItem)
-        //document.getElementById('header').style.display = "none"
         document.getElementById('content').style.display = "none"
         document.getElementById('pokemon-content').style.display = "block"
     
@@ -138,12 +110,3 @@ function backList() {
     document.getElementById('content').style.display = "block"
 }
 
-function showItemAbout() {
-    document.getElementsByClassName('t-base').removeAttribute('active')
-    document.getElementsByClassName('t-about').toggleAttribute('active')
-}
-
-function showItemBase() {
-    document.getElementsByClassName('t-about').removeAttribute('active')
-    document.getElementsByClassName('t-base').toggleAttribute('active')
-}
